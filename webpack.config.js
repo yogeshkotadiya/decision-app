7128f31823ae16c03dc138d4a7c60b1a985fb1cb
Yogesh Kotadiya <yogeshkotadiya007@gmail.com>
2018-02-12 10:26:34 +0530

    Build production bundle with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,52 @@
 // Webpack config file
 const path = require('path');
+const ExtractPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry : './src/scripts/main.js',
-    output : {
-        path : path.join(__dirname, 'public'),
-        filename : 'app.bundle.js'
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSS = new ExtractPlugin('styles.css');
+
+  return {
+    entry: './src/scripts/index.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'app.bundle.js',
     },
 
-    module : {
-        rules : [{
-            loader : 'babel-loader',
-            test : /\.js$/,
-            exclude : /node_modules/
-        },{
-            test : /\.s?css$/,
-            use : [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: CSS.extract({
+            use: [
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true,
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                      sourceMap: true,
+                    }
+                }],
+          }),
+        },
+      ],
     },
-    devtool : 'cheap-module-eval-source-map',
+    plugins: [
+        CSS
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
 
-    devServer : {
-        contentBase : path.join(__dirname, 'public')
-    }
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+    },
+  };
 };
